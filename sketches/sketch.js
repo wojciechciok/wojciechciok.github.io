a51f25166eb28a3956a8774cbd9bee02f6d366a0
Wojciech <wciokos@gmail.com>
2020-09-17 19:56:57 +0200

    added title and sketch

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,25 +1,46 @@
-let celestials = [];
-function setup() {
-  let size = windowHeight > windowWidth ? windowWidth : windowHeight;
-  createCanvas(size, size);
-  background(0);
-  loadJSON('../sketches/solar.json', function(celestial_data) {
-    let spacing = width / celestial_data.length / 2;
-    for (let i = 0; i < celestial_data.length; i++) {
-      let c = celestial_data[i];
-      celestials.push(new Celestial(c.radius * spacing / 60, i * spacing, c.color, c.year, c.name));
-    }
-  });
-}
-
-function draw() {
-  background(0);
-  translate(width / 2, height / 2);
-
-  for (let c of celestials) {
-    push();
-    rotate((-frameCount * 5) / c.year + c.offset);
-    c.show();
-    pop();
+let solar = function (p) {
+  let celestials = [];
+  p.setup =  function() {
+    let size = $("#solar").parent().width()
+    p.createCanvas(size, size);
+    p.background(0);
+    p.loadJSON('/sketches/solar.json', function(celestial_data) {
+      let spacing = p.width / celestial_data.length / 2;
+      for (let i = 0; i < celestial_data.length; i++) {
+        let c = celestial_data[i];
+        celestials.push(new Celestial(c.radius * spacing / 60, i * spacing, c.color, c.year, c.name, p));
+      }
+    });
   }
+  
+  p.draw = function() {
+    p.background(0);
+    p.translate(p.width / 2, p.height / 2);
+  
+    for (let c of celestials) {
+      p.push();
+      p.rotate((-p.frameCount * 5) / c.year + c.offset);
+      c.show();
+      p.pop();
+    }
+  }
+
+  class Celestial {
+    constructor(radius, distance, color, year) {
+      this.radius = radius;
+      this.distance = distance;
+      this.color = color;
+      this.angle = 0;
+      this.year = year;
+      this.offset = p.random(0, p.TWO_PI);
+    }
+    show() {
+      p.noStroke();
+      p.fill(this.color);
+      p.ellipse(this.distance, 0, this.radius, this.radius);
+    }
+  }
+
 }
+
+let solarP5 = new p5(solar, 'solar');
