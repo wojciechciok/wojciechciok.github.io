a9fd2235f2862a2673baa54f72816ccba2171727
Wojciech <wciokos@gmail.com>
2020-09-20 20:51:15 +0200

    added experience

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ function getMyAge() {
     let endDate  = new Date();
     let seconds = parseInt((endDate.getTime() - startDate.getTime()) / 1000);
     $('.myAge').each(function(i) {
-        $(this).html(`I am ${seconds} seconds old`);
+        $(this).html(`I am ${seconds} seconds old at the moment.`);
     })
 }
 getMyAge();
@@ -30,12 +30,10 @@ function initializeFadeIn() {
         $('.hide').each( function(i) {
             
             let top_of_object = $(this).position().top;
-            let middle_of_object = $(this).position().top + $(this).height()/2;
             let most_of_window = $(window).scrollTop() + $(window).height()*3/4;
-            let bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( top_of_object < most_of_window && middle_of_object < bottom_of_window) {
+            if( top_of_object < most_of_window) {
                 $(this).animate({'opacity':'1'},1500); 
             }
         }); 
