a3594d31af00ff191a3e8887e918ed2aa2a8a958
Wojciech Ciok <33331400+wojciechciok@users.noreply.github.com>
2021-11-03 02:03:19 +0100

    Set up nextjs with tailwind

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black">
+      <Head>
+        <title>Wojciech Ciok</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center"></main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t"></footer>
+    </div>
+  );
+}
